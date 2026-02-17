8b54976ea1b5a88344f2b13ce4e2cac9140eac0f
Marcos2593 <mromjim783@g.educaand.es>
2026-02-17 20:36:07 +0100

    Creada clase Lectura y Mapper

--- src/main/java/com/jaroso/proyectosensores/entities/Lectura.java
+++ src/main/java/com/jaroso/proyectosensores/entities/Lectura.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @ToString
-public class Lecturas {
+public class Lectura {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false)
--- src/main/java/com/jaroso/proyectosensores/mappers/LecturaMapper.java
+++ src/main/java/com/jaroso/proyectosensores/mappers/LecturaMapper.java
@@ -1,7 +1,14 @@
 package com.jaroso.proyectosensores.mappers;
 
+import com.jaroso.proyectosensores.dto.LecturaCreateDto;
+import com.jaroso.proyectosensores.dto.LecturaDto;
+import com.jaroso.proyectosensores.entities.Lectura;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
-public class LecturaMapper {
+public interface LecturaMapper {
+
+    LecturaDto toDto (Lectura lectura);
+     Lectura toEntity (LecturaCreateDto lecturaDto);
+
 }
