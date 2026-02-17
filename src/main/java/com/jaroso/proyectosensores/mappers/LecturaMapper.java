ac914b2855b4f9af4b9a97be946275b8e16cc375
Marcos2593 <mromjim783@g.educaand.es>
2026-02-17 21:20:29 +0100

    Cambio LecturaMapper

--- src/main/java/com/jaroso/proyectosensores/mappers/LecturaMapper.java
+++ src/main/java/com/jaroso/proyectosensores/mappers/LecturaMapper.java
@@ -4,11 +4,15 @@ import com.jaroso.proyectosensores.dto.LecturaCreateDto;
 import com.jaroso.proyectosensores.dto.LecturaDto;
 import com.jaroso.proyectosensores.entities.Lectura;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface LecturaMapper {
 
+    @Mapping(source = "sensor.id", target = "sensorId")
     LecturaDto toDto (Lectura lectura);
+
+    @Mapping(target = "sensor", ignore = true)
     Lectura toEntity (LecturaCreateDto lecturaDto);
 
 }
