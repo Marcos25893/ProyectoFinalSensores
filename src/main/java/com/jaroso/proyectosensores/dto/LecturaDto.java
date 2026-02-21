f59bc61cc3b45b613bf7b84d4c74bf5a9d09d9b0
Marcos2593 <mromjim783@g.educaand.es>
2026-02-21 19:30:18 +0100

    Lectura Dto con LocalDateTime arreglado

--- src/main/java/com/jaroso/proyectosensores/dto/LecturaDto.java
+++ src/main/java/com/jaroso/proyectosensores/dto/LecturaDto.java
@@ -2,5 +2,5 @@ package com.jaroso.proyectosensores.dto;
 
 import java.time.LocalDateTime;
 
-public record LecturaDto(Long id, Long sensorId, Double valor, String unidad, LocalDateTime fechaHora) {
+public record LecturaDto(Long id, Long sensorId, Double valor, String unidad, LocalDateTime timestamp) {
 }
