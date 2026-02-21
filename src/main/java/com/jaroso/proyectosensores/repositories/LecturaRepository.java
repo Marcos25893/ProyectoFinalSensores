8be7bd29e669e63fe2898a307e181c9d284b8447
Marcos2593 <mromjim783@g.educaand.es>
2026-02-21 18:44:33 +0100

    Lectura repository con findBySensor y findBetween

--- src/main/java/com/jaroso/proyectosensores/repositories/LecturaRepository.java
+++ src/main/java/com/jaroso/proyectosensores/repositories/LecturaRepository.java
@@ -4,6 +4,13 @@ import com.jaroso.proyectosensores.entities.Lectura;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface LecturaRepository extends JpaRepository<Lectura, Long> {
+
+    List<Lectura> findLecturasBySensorId(Long sensorId);
+
+    List<Lectura> findLecturasBySensorBetweenFecha(Long sensorId, LocalDateTime fechaDesde, LocalDateTime fechaHasta);
 }
